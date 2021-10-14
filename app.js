c1e122e70d67119f17a47e972ca88bbdd187d872
Maria Sori <mariasori.school@gmail.com>
2021-10-13 22:32:32 -0400

    when statement

--- app.js
+++ app.js
@@ -2,39 +2,52 @@ const inquirer = require('inquirer');
   
 const promptUser = () => {
   return inquirer.prompt([
-      {
-        type: 'input',
-        name: 'name',
-        message: 'What is your name? (Required)',
-        validate: nameInput => {
-          if (nameInput) {
-            return true;
-          } else {
-            console.log('Please enter your name!');
-            return false;
-          }
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name? (Required)',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your name!');
+          return false;
         }
-      },
-      {
-        type: 'input',
-        name: 'github',
-        message: 'Enter your GitHub Username',
-        validate: githubInput => {
-          if (githubInput) {
-            return true;
-          } else {
-            console.log('Please enter your GitHub Username!');
-            return false;
-          }
-        }
-      },
-      {
-        type: 'input',
-        name: 'about',
-        message: 'Provide some information about yourself'
       }
-    ]);
-  };
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username',
+      validate: githubInput => {
+        if (githubInput) {
+          return true;
+        } else {
+          console.log('Please enter your GitHub Username!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'confirm',
+      name: 'confirmAbout',
+      message: 'Would you like to enter some information about yourself for an "About" section?',
+      default: 'true'
+    },
+    {
+      type: 'input',
+      name: 'about',
+      message: 'Provide some information about yourself',
+      when: ({ confirmAbout }) => {
+        if (confirmAbout) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    }
+  ]);
+};
 
 const promptProject = portfolioData => {
   console.log(`
@@ -120,6 +133,7 @@ promptUser()
   .then(portfolioData => {
     console.log(portfolioData);
   });
+
 //const fs = require('fs');
 //const generatePage = require('./src/page-template');
 //
